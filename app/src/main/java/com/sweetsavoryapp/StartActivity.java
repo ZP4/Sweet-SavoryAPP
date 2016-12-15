8ab4fd8496ea9f8192752a75f546b2dc9e651c7b
ZP4 <jasonhuzheming9@gmail.com>
2016-12-15 10:02:14 -0500

    asd

--- app/src/main/java/com/sweetsavoryapp/StartActivity.java
+++ app/src/main/java/com/sweetsavoryapp/StartActivity.java
@@ -1,9 +1,54 @@
 package com.sweetsavoryapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
 /**
  * Created by ZP4 on 12/14/2016.
  */
 
-public class StartActivity {
-    
+public class StartActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        //final ImageView img = (ImageView) findViewById();
+
+        final Intent i = new Intent(this, MainActivity.class);
+        fadeOutAndHideImage(img, i);
+
+    }
+    public void fadeOutAndHideImage(final ImageView img, final Intent x) {
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setDuration(2500);
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationEnd(Animation animation) {
+                img.setVisibility(View.GONE);
+                //StartActivity(x);
+
+
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            public void onAnimationStart(Animation animation) {
+            }
+        });
+
+        img.startAnimation(fadeOut);
+
+    }
+
 }
+
