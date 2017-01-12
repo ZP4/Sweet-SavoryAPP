a3511452263f3f477326592ca0810d2ba45f28a1
Unknown <yanganthony212@gmail.com>
2017-01-12 10:03:28 -0500

    finished onclick listener for all of the buttons

--- app/src/main/java/com/sweetsavoryapp/MainActivity.java
+++ app/src/main/java/com/sweetsavoryapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.sweetsavoryapp;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +15,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Sweet N Savory");
+        Button Brooklyn =(Button) findViewById(R.id.button1);
+        Brooklyn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+        Button Hispanic =(Button) findViewById(R.id.button2);
+        Hispanic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button TheME =(Button) findViewById(R.id.button3);
+        TheME.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button Asia =(Button) findViewById(R.id.button4);
+        Asia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button Carribean =(Button) findViewById(R.id.button5);
+        Carribean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button Europe =(Button) findViewById(R.id.button6);
+        Europe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button Africa =(Button) findViewById(R.id.button7);
+        Africa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button America =(Button) findViewById(R.id.button8);
+        America.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
 
 
     }
