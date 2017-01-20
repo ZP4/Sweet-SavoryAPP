ea10a48b795cca58faca5661fa8db40e84a9000b
ZP4 <jasonhuzheming9@gmail.com>
2017-01-20 09:50:37 -0500

    contactus

---
+++ app/src/main/java/com/sweetsavoryapp/ContactUs.java
@@ -0,0 +1,14 @@
+package com.sweetsavoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+    }
+}
