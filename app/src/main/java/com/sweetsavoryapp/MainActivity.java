452b391aadabc30fd8153d90d621183b28ac3cb7
ZP4 <jasonhuzheming9@gmail.com>
2017-01-22 11:49:18 -0500

    BOTH INTENT AND ACTIVTY ARE IN MAINACTIVITY BOTH ARE COMMENTED OUT

--- app/src/main/java/com/sweetsavoryapp/MainActivity.java
+++ app/src/main/java/com/sweetsavoryapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.sweetsavoryapp;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -41,11 +42,14 @@ public class MainActivity extends AppCompatActivity {
         final Intent europeIntent = new Intent(this, European.class);
         final Intent africaIntent = new Intent(this, African.class);
         final Intent contactUs = new Intent(this, ContactUs.class);
+        //final Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
 
         Button Brooklyn =(Button) findViewById(R.id.button1);
         Brooklyn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //startActivity(browserIntent);
                 startActivity(brooklynIntent);
             }
         });
